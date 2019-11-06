02ca498008e3dd9c8ce0db02586144d10836ab8b
Brad Powell <bpowell461@gmail.com>
2019-11-05 22:09:12 -0500

    Upload New File

---
+++ php/authenticate.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+
+	die ('Please fill both the username and password field!');
+}
+
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+
+	$stmt->store_result();
+}
+	if ($stmt->num_rows > 0) {
+	$stmt->bind_result($id, $password);
+	$stmt->fetch();
+
+	if (password_verify($_POST['password'], $password)) {
+
+		session_regenerate_id();
+		$_SESSION['loggedin'] = TRUE;
+		$_SESSION['loginSucess'] = TRUE;
+		$_SESSION['name'] = $_POST['username'];
+		$_SESSION['id'] = $id;
+		header('Location: home.php');
+	} else {
+		$_SESSION['loginSucess'] = FALSE;
+		header('Location: login.php');
+	}
+} else {
+	$_SESSION['loginSucess'] = FALSE;
+	header('Location: login.php');
+}
+$stmt->close();
+?>
+
