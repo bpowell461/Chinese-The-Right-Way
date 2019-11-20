9beb2992afd7c598a1f9226ed2a300a5d600d831
Brad Powell <bpowell461@gmail.com>
2019-11-20 00:08:25 -0500

    Replace register.php

--- php/register.php
+++ php/register.php
@@ -16,7 +16,16 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $AccType = mysqli_real_escape_string($db, $_POST['AccType']);
 
+	if(isset($_POST['AccType']) && $_POST['AccType'] == 'Yes') 
+	{
+    $AccType = TRUE;
+	}
+	else
+	{
+    $AccType = FALSE;
+	}	 
 
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
@@ -43,10 +52,10 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = password_hash($password_1, PASSWORD_DEFAULT);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO accounts (username, password, email) 
-  			  VALUES('$username', '$password', '$email')";
+  	$query = "INSERT INTO accounts (username, password, email, isTeacher) 
+  			  VALUES('$username', '$password', '$email', '$AccType')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
 	$_SESSION['created'] = TRUE;
